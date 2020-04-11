b2c731045fad8371949b08874a94da1fa0316ea3
Richard W. R. Darling, Ph. D <probabilist.us@gmail.com>
2020-04-10 23:30:30 -0400

    Add files via upload
    k-focus in progress

--- prank2xy/main/algorithms/CohesionGraphBuilder.java
+++ prank2xy/main/algorithms/CohesionGraphBuilder.java
@@ -7,10 +7,11 @@
  */
 package algorithms;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.SortedSet;
@@ -32,12 +33,13 @@ public class CohesionGraphBuilder<V> {
 	 * Key set of friends consist of the points The friends will be passed in
 	 * unsorted. Sorting will occur
 	 */
+	final List<V> points; // remove later
 	final Map<V, SortedSet<V>> friends; // keyset = points; each value set has exactly K elements
 	final Map<V, Set<V>> coFriends;
 	final int k;
 	Function<V, Comparator<V>> crs; // concordant ranking system on the set of points
-	MutableValueGraph<V, Integer> focusGraph; // edge x->y carries # elements |V_{x,y}|
-	MutableValueGraph<V, Double> cohesionGraph; // edge x->y carries cohesion value
+	MutableValueGraph<V, Integer> focusGraph; // undirected edge {x,y} carries # elements |V_{x,y}|
+	MutableValueGraph<V, Double> cohesionGraph; // arc x->y carries cohesion value
 	ToIntBiFunction<V, V> kFocus;
 	// V_{x, y} K-focus counts
 
@@ -48,18 +50,20 @@ public class CohesionGraphBuilder<V> {
 		/*
 		 * The sorted set is cast as UNMODIFIABLE
 		 */
+		this.points = new ArrayList<V>(neighborSets.keySet()); // toss this
 		this.friends = neighborSets.keySet().parallelStream().collect(
 				Collectors.toMap(Function.identity(), x -> Collections.unmodifiableSortedSet(neighborSets.get(x))));
-		Iterator<V> it = neighborSets.keySet().iterator();
-		this.k = neighborSets.get(it.next()).size();
+		this.k = this.friends.get(this.points.get(0)).size();
 		this.crs = rankingSystem;
 		// Initialize
 		this.coFriends = this.transpose(neighborSets);
-		this.focusGraph = ValueGraphBuilder.directed().expectedNodeCount(neighborSets.keySet().size()).build();
+		this.focusGraph = ValueGraphBuilder.undirected().expectedNodeCount(neighborSets.keySet().size()).build();
+		this.buildFocusGraph(); // contains statistics needed for cohesion graph building
+		this.cohesionGraph = ValueGraphBuilder.directed().expectedNodeCount(neighborSets.keySet().size()).build();
 		/*
-		 * Given (x,y) return # elements |V_{x,y}| If y is not a friend of x, the answer
-		 * is k + 1. If y is a freind of x, and x prefers b other elements to y, answer
-		 * is b + 2. The 2 refers to {x, y}.
+		 * Given {x,y} return # elements |V_{x,y}|.
+		 * Compute this
+		 * i
 		 */
 		this.kFocus = (x, y) -> {
 			if (!this.friends.get(x).contains(y)) {
@@ -68,9 +72,6 @@ public class CohesionGraphBuilder<V> {
 				return Integer.valueOf(2 + this.friends.get(x).headSet(y).size());
 			}
 		};
-		this.buildFocusGraph(); // contains statistics needed for cohesion graph building
-		this.cohesionGraph = ValueGraphBuilder.directed().expectedNodeCount(neighborSets.keySet().size()).build();
-
 	}
 
 	ToDoubleFunction<EndpointPair<V>> cohesion = e -> {
@@ -94,10 +95,26 @@ public class CohesionGraphBuilder<V> {
 				.collect(Collectors.toMap(Function.identity(), y -> Collections.unmodifiableSet(inArcs.get(y))));
 	}
 
+	/*
+	 * Since graph is undirected, it suffices to traverse unordered pairs {x, y}
+	 * UNFINISHED
+	 */
 	private void buildFocusGraph() {
+		int xRanksY, yRanksX;
 		for (V x : this.friends.keySet()) {
+			xRanksY = 0; // how many friends z satisfy r_x(z) < r_x(y), excluding x
 			for (V y : this.friends.get(x)) {
-				this.focusGraph.putEdgeValue(x, y, this.kFocus.applyAsInt(x, y));
+				// Case 0: x is not a friend of y
+				if(!this.friends.get(y).contains(x)) {
+					
+				}
+					
+					// how many z satisfy r_y(z) < r_y(x), excluding y
+				yRanksX = this.friends.get(y).contains(x)? this.friends.get(y).headSet(x).size() : this.k;
+				
+				
+				this.focusGraph.putEdgeValue(x, y, 0);
+				xRanksY++;
 			}
 		}
 	}
